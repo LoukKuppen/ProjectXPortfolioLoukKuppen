5f68c704f7ae8ca734cb979a342a4c71a9db148f
Louk Kuppen <563949@student.fontys.nl>
2025-06-03 11:24:01 +0200

    BoombapGenreV1

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ document.querySelectorAll('.genre-card').forEach(card => {
         window.location.href = card.getAttribute('data-link');
     });
 });
+
+document.querySelectorAll('.genre-card').forEach(card => {
+    card.addEventListener('click', () => {
+        window.location.href = card.getAttribute('data-link');
+    });
+
+    const video = card.querySelector('.genre-video');
+    if (video) {
+        card.addEventListener('mouseenter', () => {
+            video.currentTime = 0;
+            video.play();
+        });
+        card.addEventListener('mouseleave', () => {
+            video.pause();
+            video.currentTime = 0;
+        });
+    }
+});
